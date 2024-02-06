1670cf174115efaee711d60f201daeedb6ece526
Capy <cgrothaus@outlook.com>
2024-02-05 21:02:17 -0500

    move non private method up to other none private messages

--- src/classes/DiscordManager.ts
+++ src/classes/DiscordManager.ts
@@ -68,6 +68,43 @@ export default class DiscordManager {
     process.exit(1);
   }
 
+  async setSlashCommands(guildId: string): Promise<boolean> {
+    const {CLIENT_ID, DISCORD_TOKEN} = process.env;
+    const commands: Array<RESTPostAPIChatInputApplicationCommandsJSONBody> = [];
+    // Grab all the command folders from the commands directory you created earlier
+    const commandsPath = path.join(__dirname, 'src/commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+
+    for (const file of commandFiles) {
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+      const filePath = path.join(commandsPath, file);
+      const command: Command = await import(filePath);
+      if (command.data && command.execute) {
+        commands.push(command.data.toJSON());
+      } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+      }
+    }
+  
+    const rest = new REST().setToken(DISCORD_TOKEN as string);
+  
+    try {
+      console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+      const data = await rest.put(
+        Routes.applicationGuildCommands(CLIENT_ID as string, guildId),
+        { body: commands },
+      ) as Array<any>;
+  
+      console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+
+    return true;
+  }
+
   private async createSwitchMessage(switchEntity: SwitchFcmNotification): Promise<Message> {
     const onButton = new ButtonBuilder()
       .setCustomId(switchEntity.entityId + '-on')
@@ -215,43 +252,6 @@ export default class DiscordManager {
         this.refreshMessages();
       }
     });
-  }
-
-  async setSlashCommands(guildId: string): Promise<boolean> {
-    const {CLIENT_ID, DISCORD_TOKEN} = process.env;
-    const commands: Array<RESTPostAPIChatInputApplicationCommandsJSONBody> = [];
-    // Grab all the command folders from the commands directory you created earlier
-    const commandsPath = path.join(__dirname, 'src/commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
-
-    for (const file of commandFiles) {
-    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-      const filePath = path.join(commandsPath, file);
-      const command: Command = await import(filePath);
-      if (command.data && command.execute) {
-        commands.push(command.data.toJSON());
-      } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-      }
-    }
-  
-    const rest = new REST().setToken(DISCORD_TOKEN as string);
-  
-    try {
-      console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-      const data = await rest.put(
-        Routes.applicationGuildCommands(CLIENT_ID as string, guildId),
-        { body: commands },
-      ) as Array<any>;
-  
-      console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
-
-    return true;
   }
 
   private registerRustPlusListeners(): void {
