76a70b5d4b4d9717d2b58da1e4c31de950648623
Capy <cgrothaus@outlook.com>
2024-02-23 20:21:19 -0500

    CommandManager tests

--- src/classes/CommandManager.ts
+++ src/classes/CommandManager.ts
@@ -1,61 +1,61 @@
-import { Collection, REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord.js';
+import { REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from 'discord.js';
 import Command from './Command';
 import path from 'path';
 import fs from 'fs';
 
 export default class CommandManager {
 
-  commands: Collection<string, Command>;
+  commands: Array<RESTPostAPIChatInputApplicationCommandsJSONBody> = [];
+
+  private guildId: string;
 
   constructor(guildId: string) {
-    this.loadCommands();
-    this.setSlashCommands(guildId);
+    this.guildId = guildId;
   }
 
-  private async setSlashCommands(guildId: string): Promise<boolean> {
-    const { APPLICATION_ID, DISCORD_TOKEN } = process.env;
-    const commands: Array<RESTPostAPIChatInputApplicationCommandsJSONBody> = [];
-    // Grab all the command folders from the commands directory you created earlier
-    const commandsPath = path.join(__dirname, '../commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
-
-    for (const file of commandFiles) {
-    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-      const filePath = path.join(commandsPath, file);
-      const command: Command = await import(filePath);
-      if (command.data && command.execute) {
-        commands.push(command.data.toJSON());
-      } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  async loadCommands(): Promise<void> {
+    try {
+      // Grab all the command folders from the commands directory you created earlier
+      const commandsPath = path.join(__dirname, '../commands');
+      const commandFiles = fs.readdirSync(commandsPath);
+      for (const file of commandFiles) {
+        // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+        const filePath = path.join(commandsPath, file);
+        const command: Command = await import(filePath);
+        if (command.data && command.execute) {
+          this.commands.push(command.data.toJSON());
+        } else {
+          throw new Error(`The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
       }
+    } catch (error) {
+      throw new Error(`Failed to load commands: ${error.message}`);
+    }
+
+    await this.postCommands();
+  }
+
+  private async postCommands(): Promise<void> {
+    const { APPLICATION_ID, DISCORD_TOKEN } = process.env;
+    if (!APPLICATION_ID) {
+      throw new Error('Missing required application ID.');
+    } else if (!DISCORD_TOKEN) {
+      throw new Error('Missing required discord token.');
+    } else if (!this.guildId) {
+      throw new Error('Missing required guild ID.');
+    } else if (!this.commands.length) {
+      throw new Error('No commands loaded.');
     }
 
     const rest = new REST().setToken(DISCORD_TOKEN as string);
 
     try {
       await rest.put(
-        Routes.applicationGuildCommands(APPLICATION_ID as string, guildId),
-        { body: commands }
+        Routes.applicationGuildCommands(APPLICATION_ID as string, this.guildId),
+        { body: this.commands }
       ) as Array<any>;
     } catch (error) {
-      console.error(error);
-      return false;
-    }
-
-    return true;
-  }
-
-  private async loadCommands(): Promise<void> {
-    const commandsPath = path.join(__dirname, '../commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
-
-    for (const file of commandFiles) {
-    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-      const filePath = path.join(commandsPath, file);
-      const command: Command = await import(filePath);
-      if (command.data && command.execute) {
-        this.commands.set(command.data.name, command);
-      }
+      throw new Error(`Failed to register application commands: ${error.message}`);
     }
   }
 }
